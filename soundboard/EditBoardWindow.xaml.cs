91aafcb45108806fc010164c171d6ed483fb8578
Matt Thomson <matt.thomson@cantab.net>
2011-11-03 20:05:42 +0000

    Bind edit window to board.

--- soundboard/EditBoardWindow.xaml.cs
+++ soundboard/EditBoardWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.ComponentModel;
+using SoundBoard.Model;
 
 namespace SoundBoard
 {
@@ -27,6 +17,7 @@ namespace SoundBoard
         public EditBoardWindow()
         {
             InitializeComponent();
+            SoundsList.DataContext = BoardHandler.Instance;
         }
         #endregion
 
