d29a1de215c29753892de1ef1f4b2988793855ef
Matt Thomson <matt.thomson@cantab.net>
2011-11-09 18:06:40 +0000

    Change directory structure.

--- soundboard/src/properties/Resources.Designer.cs
+++ soundboard/src/properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SoundBoard.Properties {
+namespace SoundBoard.src.properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SoundBoard.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SoundBoard.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SoundBoard.src.properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
