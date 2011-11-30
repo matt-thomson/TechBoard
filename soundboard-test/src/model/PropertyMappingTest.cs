0a09b894d652983ca4b1609bd116abd94a6a2f67
Matt Thomson <matt.thomson@cantab.net>
2011-11-30 19:19:34 +0000

    A rather silly test.

---
+++ soundboard-test/src/model/PropertyMappingTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace SoundBoard.Model.Test
+{
+    [TestFixture]
+    public class PropertyMappingTest
+    {
+        #region Public properties
+        public string MyProperty { get; set; }
+        #endregion
+
+        #region Private members
+        // Object under test
+        private PropertyMapping mMapping;
+        private PropertyInfo mPropInfo;
+        #endregion
+
+        [SetUp]
+        public void TestInit()
+        {
+            // Set up the mapping.
+            mPropInfo = this.GetType().GetProperty("MyProperty");
+            mMapping = new PropertyMapping(mPropInfo, this);
+        }
+
+        [Test]
+        public void TestProperty()
+        {
+            Assert.AreEqual(mPropInfo, mMapping.Property);
+        }
+
+        [Test]
+        public void TestTarget()
+        {
+            Assert.AreEqual(this, mMapping.Target);
+        }
+    }
+}
