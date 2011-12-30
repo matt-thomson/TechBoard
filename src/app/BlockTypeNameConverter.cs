fea11c374ca260fdde4f0d00ebe53902f75c3918
Matt Thomson <matt.thomson@cantab.net>
2011-12-30 19:52:44 +0000

    Tidy up views.

---
+++ src/app/BlockTypeNameConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace SoundBoard.App
+{
+    public class BlockTypeNameConverter : IValueConverter
+    {
+        public object Convert(object xiValue,
+                                  Type xiTargetType,
+                                  object xiParameter,
+                                  CultureInfo xiCulture)
+        {
+            // Extract the type name.
+            Type type = xiValue as Type;
+
+            if (type == null)
+            {
+                type = xiValue.GetType();
+            }
+
+            string result = type.Name;
+
+            // If the type name ends in "Block", strip this off.
+            if (result.EndsWith("Block"))
+            {
+                result = result.Substring(0, result.Length - 5);
+            }
+
+            // Convert to camel case.
+            result = Regex.Replace(result,
+                                   "([A-Z]+[a-z]*)",
+                                   " $1",
+                                   RegexOptions.Compiled).Trim();
+            
+            return result;
+        }
+
+        public object ConvertBack(object xiValue,
+                                  Type xiTargetType,
+                                  object xiParameter,
+                                  CultureInfo xiCulture)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/app/PropertyNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace SoundBoard.App
+{
+    public class PropertyNameConverter : IValueConverter
+    {
+        public object Convert(object xiValue,
+                              Type xiTargetType,
+                              object xiParameter,
+                              CultureInfo xiCulture)
+        {            
+            string result = xiValue as string;
+            result = Regex.Replace(result, 
+                                   "([A-Z]+[a-z]*)",
+                                   " $1", 
+                                   RegexOptions.Compiled).Trim();
+            return result;
+        }
+
+        public object ConvertBack(object xiValue,
+                                  Type xiTargetType,
+                                  object xiParameter,
+                                  CultureInfo xiCulture)
+        {
+            return null;
+        }
+    }
+}
