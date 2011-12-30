d2dcada101a70fd92a1608adb25e60fd9503340b
Matt Thomson <matt.thomson@cantab.net>
2011-12-30 23:02:31 +0000

    Add new plugins, and text block type.

---
+++ src/plugins/basic/TextBlock.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SoundBoard.Plugins.Basic
+{
+    /// <summary>
+    /// Interaction logic for SoundBlock.xaml
+    /// </summary>
+    [Block("{6A2910EE-BDD9-453A-87F4-7994AF4223AD}")]
+    public partial class TextBlock : UserControl
+    {
+        #region Dependency properties
+        private static DependencyProperty TextProperty = DependencyProperty.Register("Text",
+                                                                                     typeof(string),
+                                                                                     typeof(TextBlock));
+        #endregion
+
+        #region Public properties
+        [TextBlockProperty]
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public TextBlock()
+        {
+            // Initialize.
+            InitializeComponent();
+
+            // Set up the default properties.
+            Text = "New Text";
+        }
+        #endregion
+    }
+}
