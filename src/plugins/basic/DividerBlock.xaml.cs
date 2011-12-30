feb26b1470f13c8f187e6f54b0de8219466b50ee
Matt Thomson <matt.thomson@cantab.net>
2011-12-30 23:27:59 +0000

    Add divider block.

---
+++ src/plugins/basic/DividerBlock.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace SoundBoard.Plugins.Basic
+{
+    /// <summary>
+    /// Interaction logic for DividerBlock.xaml
+    /// </summary>
+    [Block("{6127E146-1C7A-44EC-AF0D-F7063917E581}")]
+    public partial class DividerBlock : UserControl
+    {
+        #region Constructor
+        public DividerBlock()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
