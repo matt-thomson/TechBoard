6b329928f6d3c35c66f198bc43a70961b997b9ff
Matt Thomson <matt.thomson@cantab.net>
2011-12-31 12:56:41 +0000

    Tidy up.

---
+++ src/plugins/time/ClockBlock.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SoundBoard.Plugins.Time
+{
+    /// <summary>
+    /// Interaction logic for ClockBlock.xaml
+    /// </summary>
+    [Block("{315E02FB-E665-4516-B6E8-871A540B747C}")]
+    public partial class ClockBlock : UserControl
+    {
+        #region Static members
+        private static DispatcherTimer mTimer = new DispatcherTimer();
+        #endregion
+
+        #region Constructor
+        public ClockBlock()
+        {
+            // Initialize.
+            InitializeComponent();
+
+            // Set the timer.
+            mTimer.Interval = new TimeSpan(0, 0, 1);
+            mTimer.Tick += HandleTimerElapsed;
+            mTimer.Start();
+
+            // Set the initial time.
+            Time.Content = DateTime.Now.ToString("T");
+        }
+        #endregion
+
+        #region Event handlers
+        private void HandleTimerElapsed(object sender, EventArgs e)
+        {
+            Time.Content = DateTime.Now.ToString("T");
+        }
+        #endregion
+    }
+}
