439ee3dd52979cc79c8c3289582d7bce542db535
Matt Thomson <matt.thomson@cantab.net>
2012-01-14 14:09:59 +0000

    GPL-ify.

--- src/app/EditBoardWindow.xaml.cs
+++ src/app/EditBoardWindow.xaml.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+ *  This file is part of TechBoard.
+ *  Copyright (C) 2011-2012 Matt Thomson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  For more information on TechBoard, see 
+ *  <http://www.matt-thomson.co.uk/software/techboard>.
+ */
+
+using System;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
